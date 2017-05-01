e8e04009721d5e1bb52b0c5d5f8409942c8b3d7e
mnipoiperi <mnipoiperi@gmail.com>
2017-05-01 14:09:06 +0800

    first version

---
+++ Assets/playvideo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playvideo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Handheld.PlayFullScreenMovie("Gondola Ride In Venice.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
